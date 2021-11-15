06c4b93bdc894e8e9dec4a51388461d8b9b2319d
HuxyUK <HuxyUK@users.noreply.github.com>
2021-11-15 23:44:25 +0000

    ➕ [Libs] Add stduuid support to ASGE

---
+++ cmake/libs/stduuid.cmake
@@ -0,0 +1,25 @@
+message(STATUS  "STDUUID C++17:")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+message(VERBOSE "STDUUID C++17 IMPLEMENTATION OF PROPOSAL P0959")
+
+include(FetchContent)
+message(STATUS "+ FETCHING STDUUID....")
+FetchContent_Declare(
+        stduuid
+        GIT_REPOSITORY https://github.com/mariusbancila/stduuid.git
+        GIT_TAG        origin/master)
+
+FetchContent_GetProperties(stduuid)
+if(NOT stduuid_POPULATED)
+    set(UUID_BUILD_TESTS OFF  CACHE BOOL "" FORCE)
+    message(STATUS "+ CONFIGURING STDUUID....")
+    list(APPEND CMAKE_MESSAGE_INDENT "  ")
+
+    FetchContent_MakeAvailable(stduuid)
+    add_library(stduuid::stduuid ALIAS stduuid)
+    message(DEBUG "STDUUID INCLUDE DIR: ${stduuid_SOURCE_DIR}")
+
+    list(POP_BACK CMAKE_MESSAGE_INDENT)
+    message(STATUS "+ DONE")
+    list(POP_BACK CMAKE_MESSAGE_INDENT)
+endif()
